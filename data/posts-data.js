eb86366118546ee171465dfe1bbd423e563194bd
1009811744@qq.com <email>
2020-08-20 20:01:04 +0800

    update

---
+++ data/posts-data.js
@@ -0,0 +1,32 @@
+var local_database = [{
+    date: "2020.8.19",
+    title: "库里带儿子看球",
+    content: "8月19日讯 今日，勇士后卫斯蒂芬-库里更新了个人Instagram，晒出自己与儿子卡农的合照。库里写道：“在西海岸早中晚餐的时候都能看季后赛？好活多整点。”",
+    avatar: "/images/avatar/1.png",
+    post_img: "/images/post/4.jpg",
+    read: "300",
+    collection: "123"
+  },
+  {
+    date: "2020.8.12",
+    title: "库里又跑去打高尔夫",
+    content: "8月12日讯 今日，一名勇士博主更新了Instagram，晒出勇士后卫斯蒂芬-库里在周末傍晚于哈丁公园打高尔夫的片段。",
+    avatar: "/images/avatar/2.png",
+    post_img: "/images/post/5.jpg",
+    read: "340",
+    collection: "173"
+  },
+  {
+    date: "2020.8.19",
+    title: "库里夫妇出游",
+    content: "8月5日讯 今日，勇士后卫斯蒂芬-库里的妻子阿耶莎更新了个人Instagram，晒出两张两人驾车出游时的照片。阿耶莎写道：“当一次驾车变成一次约会，喜欢一次美好的出游和一张不错的歌单。”",
+    avatar: "/images/avatar/3.png",
+    post_img: "/images/post/6.jpg",
+    read: "390",
+    collection: "223"
+  }
+]
+
+module.exports = {
+  postList: local_database
+}
